ec512c5506e4e9d5f84c3ba0a2ff7f3107fefffc
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-12-04 20:37:06 +0530

    fixed

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -2,9 +2,6 @@ import { Router, Request, Response } from "express";
 import { register, login } from "../controllers/auth-controller";
 
 const router = Router();
-router.get("/",  async (req: Request, res: Response) => {
-res.json("Working")
-});
 // login
 router.post("/register", register);
 // register
